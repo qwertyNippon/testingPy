818ef7321c83c8b97eec1d4b4a132b1078b99e4c
qwertyNippon <orlandohenry100@gmail.com>
2023-11-09 23:32:26 -0500

    fixed!!!

--- test.py
+++ test.py
@@ -1,13 +1,18 @@
 def cityList(x):
     lst=[]
-    # x = x.strip( )
     x = x.replace(' ','-')
     print(x)
-    x = x.split()
-    print(x) 
+    x = x.split(',')
+    # print(x) 
+    # print(x[0])
     for i in range(0, len(x), 4):
         lst.append(x[i])
     print(lst)
+    for i in range(len(lst)):
+        lst=','.join(lst)
+        lst=lst.replace('-', ' ')
+        lst=lst.split(',')
+    print(lst)
 
 
-cityList('les Escaldes,Andorra,Escaldes-Engordany,3040051')
+cityList('les Escaldes,Andorra,Escaldes-Engordany,3040051,Dubai,United Arab Emirates,Dubai,292223,Andorra la Vella,Andorra,Andorra la Vella,3041563')
